877c9054396de20a4ddc199be0b20fd48a0dd5de
Andre Katov <andy.katov@icloud.com>
2023-04-24 18:21:53 +0300

    added new project

---
+++ js/app.js
@@ -0,0 +1,76 @@
+const imageContainers = document.querySelectorAll(".gallery-image-container");
+const checkboxes = document.querySelectorAll(".search-checkbox");
+const body = document.querySelector("body");
+const xmarks = document.querySelectorAll(".xmark");
+
+const slidesObj = {
+  // slide position name are assuming the "from" starting position.
+  slideWD: "slide-in-up-right",
+  slideWA: "slide-in-up-left",
+  slideSD: "slide-in-bottom-right",
+  slideSA: "slide-in-bottom-left",
+  slideD: "slide-in-right",
+  slideA: "slide-in-left",
+  slideW: "slide-in-top",
+  slideS: "slide-in-bottom",
+};
+
+const randomSlideGenerator = () => {
+  const slidesArr = Object.entries(slidesObj);
+  const randomIndex = Math.floor(Math.random() * slidesArr.length);
+  return slidesArr[randomIndex][1];
+};
+
+checkboxes.forEach((checkbox) => {
+  checkbox.checked = false;
+  checkbox.addEventListener("click", (e) => {
+    const randomSlide = randomSlideGenerator();
+    const imageContainer = checkbox.nextElementSibling;
+    imageContainer.classList.toggle("zoomed-image");
+    if (e.target.checked === true) {
+      imageContainer.classList.add(randomSlide);
+      body.classList.add("no-scroll");
+    } else {
+      const slideClasses = Object.values(slidesObj);
+      imageContainer.classList.remove(...slideClasses);
+      body.classList.remove("no-scroll");
+    }
+  });
+});
+
+// ********** set date ************
+// select span
+const date = (document.getElementById("date").innerHTML =
+  new Date().getFullYear());
+
+// // ********** nav toggle ************
+// // select button and links
+// const navBtn = document.getElementById("nav-toggle");
+// const links = document.getElementById("nav-links");
+// // add event listener
+// navBtn.addEventListener("click", () => {
+//   links.classList.toggle("show-links");
+// });
+
+// // ********** smooth scroll ************
+// // select links
+// const scrollLinks = document.querySelectorAll(".scroll-link");
+// scrollLinks.forEach((link) => {
+//   link.addEventListener("click", (e) => {
+//     // prevent default
+//     e.preventDefault();
+//     links.classList.remove("show-links");
+
+//     const id = e.target.getAttribute("href").slice(1);
+//     const element = document.getElementById(id);
+//     //
+//     let position = element.offsetTop - 62;
+
+//     window.scrollTo({
+//       left: 0,
+//       // top: element.offsetTop,
+//       top: position,
+//       behavior: "smooth",
+//     });
+//   });
+// });
